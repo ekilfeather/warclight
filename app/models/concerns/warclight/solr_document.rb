f96f8751f0cde5aec3bab858799f94749009006a
Eoin Kilfeather <ekilfeather@gmail.com>
2019-07-01 11:38:54 +0100

    use APP_CONFIG for replay urls

--- app/models/concerns/warclight/solr_document.rb
+++ app/models/concerns/warclight/solr_document.rb
@@ -7,7 +7,7 @@ module Warclight
     extend Blacklight::Solr::Document
 
     def replay_link
-      time_travel_base_url = 'http://webarchiveportal.nli.ie:8080/all/timemap/json'
+      time_travel_base_url = APP_CONFIG['time_travel_base_url']
       time_travel_request_url = time_travel_base_url + first(:wayback_date).to_s + '/' + first(:url).to_s
       time_travel_request = URI(time_travel_request_url)
       time_travel_response = Net::HTTP.get(time_travel_request)
@@ -15,7 +15,7 @@ module Warclight
         time_travel_response_json = JSON.parse(time_travel_response)
         replay_url = time_travel_response_json['url']
         replay_date = time_travel_response_json['timestamp']
-        replay_url_link = '<a href="' + "http://webarchiveportal:8080/all/#{replay_date}/#{replay_url}/" '" target="_blank">'"#{replay_url}"'</a> 🔗'
+        replay_url_link = "<a href=\"#{APP_CONFIG['replay_base_url']}#{replay_date}/#{replay_url}\" target=\"_blank\">#{replay_url}\"</a>"
         replay_url_link.html_safe
       else
         replay_url = 'Not Available.'
