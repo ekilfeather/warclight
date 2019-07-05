ecd56f6cf92b532ba3d8781078247cc2b376a408
Eoin Kilfeather <ekilfeather@gmail.com>
2019-07-05 12:09:57 +0100

    deal with multiple results

--- app/models/concerns/warclight/solr_document.rb
+++ app/models/concerns/warclight/solr_document.rb
@@ -8,11 +8,14 @@ module Warclight
 
     def replay_link
       time_travel_base_url = APP_CONFIG['time_travel_base_url']
-      time_travel_request_url = time_travel_base_url + first(:url).to_s
+      timestamp = first(:wayback_date).to_s
+      time_travel_request_url = time_travel_base_url + "#{timestamp}/" + first(:url).to_s
       time_travel_request = URI(URI.encode(time_travel_request_url))
+      puts time_travel_request_url
       time_travel_response = Net::HTTP.get(time_travel_request)
       if time_travel_response.present?
-        time_travel_response_json = JSON.parse(time_travel_response)
+        time_travel_response = time_travel_response.scan(/"urlkey\"/).count > 1 ? "[#{time_travel_response.gsub! /}\n{/, '},{' }]" :"[#{time_travel_response}]"
+        time_travel_response_json = JSON.parse(time_travel_response).first
         replay_url = time_travel_response_json['url']
         replay_date = time_travel_response_json['timestamp']
         replay_url_link = "<a href=\"#{APP_CONFIG['replay_base_url']}#{replay_date}/#{replay_url}\" target=\"_blank\">#{replay_url}</a>"
